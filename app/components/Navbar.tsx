49559dc1e345f13a5800d9f1cf3b1c154543b902
Govind29-rgb <govoffi29@gmail.com>
2025-03-16 13:20:32 +0530

    logo_added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image 
+                        src="/logo.webp" 
+                        alt="logo" 
+                        width={100} 
+                        height={50} 
+                        className="rounded-full border border-gray-300 shadow-sm"
+                    />
+                </Link>
+            </nav>
+        </header>
+    );
 }
 
-export default Navbar
+export default Navbar;
